ae72400c2cd8c95ef14dab81d2acb7b8f2fc0a84
Billi Ramirez <ramirezbilly42@outlook.com>
2020-09-11 09:45:44 -0600

    rendering api data

--- functions/movies.js
+++ functions/movies.js
@@ -1,8 +1,32 @@
+require("dotenv").config();
+const { URL } = require("url");
+const fetch = require("node-fetch");
 const movies = require("../data/movies.json");
 
 exports.handler = async () => {
+  const api = new URL("https://www.omdbapi.com/");
+  // add the secret api
+  api.searchParams.set("apikey", process.env.OMDB_API_KEY);
+
+  const promises = movies.map((movie) => {
+    //   // use the movies omdbapi for look up
+    api.searchParams.set("i", movie.id);
+    return fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        const scores = data.Ratings;
+
+        return {
+          ...movie,
+          scores,
+        };
+      });
+  });
+
+  const moviesWithRatings = await Promise.all(promises);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(movies),
+    body: JSON.stringify(moviesWithRatings),
   };
 };
